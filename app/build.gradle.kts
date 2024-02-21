32e1e681fae295eda641e55ef90647aa4d9ee13a
Eduardo <eduardojor77@gmail.com>
2024-02-21 13:31:50 -0300

    Mudanças na interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.first_class_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.first_class_project"
