1f65c53a1eaa8d48a396084da35b195ccce6f302
Eduardo <eduardojor77@gmail.com>
2024-02-20 22:03:18 -0300

    logica de conversão feita

--- app/src/main/java/com/example/first_class_project/MainActivity.java
+++ app/src/main/java/com/example/first_class_project/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.first_class_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnConverter;
+    TextView labelDollar, labelEuro;
+    EditText entradaReal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        labelDollar = findViewById(R.id.txtDollar);
+        labelEuro = findViewById(R.id.txtEuro);
+        entradaReal = findViewById(R.id.edtReal);
+        btnConverter = findViewById(R.id.button);
+    }
+
+    public void converter(View v) {
+        float cambioDollar = 4.93f;
+        float cambioEuro = 5.33f;
+
+        float valorDollar, valorEuro, valorReal;
+
+        String aux = entradaReal.getText().toString();
+        valorReal = Float.parseFloat(aux);
+
+        valorDollar = valorReal / cambioDollar;
+        valorEuro = valorReal / cambioEuro;
+
+        labelDollar.setText(String.format("%.2f", valorDollar));
+        labelEuro.setText(String.format("%.2f", valorEuro));
+
     }
 }
